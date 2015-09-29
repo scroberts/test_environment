354494ef5fe496a943c89fd2b6caec18bccb2f4d
Paul Barnes <barnesp@hawaii.edu>
2015-09-28 16:14:03 -1000

    first commit

---
+++ example.py
@@ -0,0 +1,4 @@
+#This is a simple python program to help initiate collab with Scott
+
+print("hello world, this is python")
+print("We can use this file to demonstate code sharing and modification")
