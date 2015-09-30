6b455d1f180ed7e7714ccca6296cf61fccea85e9
scroberts <scottroberts8314@gmail.com>
2015-09-29 17:47:28 -0700

    Change to example.py
    This is a test change

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
 #This is a simple python program to help initiate collab with Scott
 
+# This is a comment from Scott
+
 print("hello world, this is python")
 print("We can use this file to demonstate code sharing and modification")
